ddfa6b0b7229da0424dc74f64883e39e95ccf527
Sebastian Plattner <sebastian.plattner@gmail.com>
2019-02-01 12:15:27 +0100

    Initial Commit

---
+++ MMM-MQTT.js
@@ -0,0 +1,56 @@
+
+Module.register("MMM-MQTT", {
+
+    // Default module config
+    defaults: {
+        mqttServers: []
+    },
+
+    makeServerKey: function (server) {
+        return '' + server.address + ':' + (server.port | '1883' + server.user);
+    },
+
+    start: function () {
+        console.log(this.name + ' started.');
+        this.subscriptions = [];
+
+        console.log(this.name + ': Setting up connection to ' + this.config.mqttServers.length + ' servers');
+
+        for (i = 0; i < this.config.mqttServers.length; i++) {
+            var s = this.config.mqttServers[i];
+            var serverKey = this.makeServerKey(s);
+            console.log(this.name + ': Adding config for ' + s.address + ' port ' + s.port + ' user ' + s.user);
+            for (j = 0; j < s.subscriptions.length; j++) {
+                var sub = s.subscriptions[j];
+                this.subscriptions.push({
+                    serverKey: serverKey,
+                    notificationKey: sub.notificationKey
+                });
+            }
+        }
+
+        this.openMqttConnection();
+        var self = this;
+    },
+
+    openMqttConnection: function () {
+        this.sendSocketNotification('MQTT_CONFIG', this.config);
+    },
+
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === 'MQTT_PAYLOAD') {
+            if (payload != null) {
+                for (i = 0; i < this.subscriptions.length; i++) {
+                    sub = this.subscriptions[i];
+                    if (sub.serverKey == payload.serverKey && sub.topic == payload.topic) {
+                        this.sendNotification(sub.notificationKey, payload.value);
+                    }
+                }
+            } else {
+                console.log(this.name + ': MQTT_PAYLOAD - No payload');
+            }
+        }
+    },
+
+
+});
---
+++ README.md
@@ -0,0 +1,35 @@
+# MQTT notification
+
+This Module sends Payload received on a MQTT Topic to a defined MM Notification
+
+
+
+
+## Configuration
+
+Here is an example configuration with description. Put it in the `MagicMirror/config/config.js` file:
+
+    {
+        module: 'MMM-MQTT',
+        position: 'bottom_left',
+        header: 'MQTT',
+        config: {
+            mqttServers: [
+                {
+                    address: 'localhost',  // Server address or IP address
+                    port: '1883',          // Port number if other than default
+                    user: 'user',          // Leave out for no user
+                    password: 'password',  // Leave out for no password
+                    subscriptions: [
+                        {
+                            topic: 'smoky/1/inside/temperature', // Topic to look for
+                            notificationKey: 'CUSTOM_NOTIFICATION' // Key of the Notification to be Send
+                        }
+                    ]
+                }
+            ],
+        }
+    }
+
+
+mqttServers is an array, so you can add multiple servers to the same config.
